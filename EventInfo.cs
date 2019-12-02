6779fe896dfd8447c6a6d65433e58bb75c5dc47f
OlexiyTelyakov <43723474+OlexiyTelyakov@users.noreply.github.com>
2019-12-02 12:09:37 +1000

    Add files via upload

---
+++ EventInfo.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KiwiKaleidoscope 
+{
+    public abstract class EventInfo 
+    {
+        //Some Info
+        //Considering the degree to which the different events rely on different information, it will be inherited by classes.
+        //As opposed to carrying any information in of itself, unless there is a need for a very very basic event.
+    }
+
+    public class InventoryEventInfo : EventInfo 
+    {
+        public bool enabledUI;
+        public bool itemDropped;
+        public List<InventorySlot> inventory;
+        public Item changedItem;
+
+        public InventoryEventInfo(List<InventorySlot> inventory, bool enabledUI, bool itemDropped, Item changedItem)
+        {
+            this.enabledUI = enabledUI;
+            this.inventory = inventory;
+            this.itemDropped = itemDropped;
+            this.changedItem = changedItem;
+        }
+    }
+
+    public class EquipItemInfo : EventInfo
+    {
+        public Item equippedItem;
+
+        public EquipItemInfo(Item equippedItem)
+        {
+            this.equippedItem = equippedItem;
+        }
+    }
+
+    public class WaterCanEventInfo : EventInfo
+    {
+        public float waterAmount;
+        public float waterCapacity;
+
+        public WaterCanEventInfo(float waterAmount, float waterCapacity)
+        {
+            this.waterAmount = waterAmount;
+            this.waterCapacity = waterCapacity;
+        }
+    }
+
+    public class ItemUsedInfo : EventInfo
+    {
+        public Item usedItem;
+        public TileGrid.Tile useTarget;
+
+        public ItemUsedInfo(Item usedItem, TileGrid.Tile useTarget)
+        {
+            this.usedItem = usedItem;
+            this.useTarget = useTarget;
+        }
+    }
+
+    public class AudioEffectInfo : EventInfo
+    {
+        public AudioClip clip;
+        public float volume;
+
+        public AudioEffectInfo(AudioClip clip, float volume = 1)
+        {
+            this.clip = clip;
+            this.volume = volume;
+        }
+    }
+
+    public class ShopEventInfo : EventInfo 
+    {
+        public IShop shop;
+        public Item item;
+        public bool soldFromShop;
+        public int itemQuantity;
+
+        public ShopEventInfo(IShop shop, Item item, bool soldFromShop, int itemQuantity)
+        {
+            this.shop = shop;
+            this.item = item;
+            this.soldFromShop = soldFromShop;
+            this.itemQuantity = itemQuantity;
+        }
+    }
+
+    public class TimeEventInfo : EventInfo
+    {
+        public bool dayOver;
+        public int dayCount;
+        public int timeOfDay;
+        public bool fromBed;
+
+        //Season change would go here.
+
+        public TimeEventInfo(bool dayOver, int dayCount, int timeOfDay, bool fromBed)
+        {
+            this.dayOver = dayOver;
+            this.dayCount = dayCount;
+            this.timeOfDay = timeOfDay;
+            this.fromBed = fromBed;
+        }
+    }
+
+    public class RoomTraversalEvent : EventInfo 
+    {
+        public Room destination;
+        public string traverserName;
+
+        public RoomTraversalEvent(string traverserName, Room destination)
+        {
+            this.traverserName = traverserName;
+            this.destination = destination;
+        }
+    }
+
+    public class KillzoneEvent: EventInfo
+    {
+        //This is a notification that doesn't require any information.
+    }
+
+    public class PestKillEvent : EventInfo
+    {
+        //This is a notification that doesn't require any information.
+    }
+}
+
+
---
+++ EventManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KiwiKaleidoscope 
+{
+    public class EventManager : MonoBehaviour 
+    {
+
+        #region Singleton
+        private static EventManager instance;
+        public static EventManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<EventManager>();
+                    if (instance == null)
+                    {
+                        GameObject go = new GameObject("EventManager");
+                        instance = go.AddComponent<EventManager>();
+                    }
+                }
+                return instance;
+            }
+        }
+        #endregion
+
+        private Dictionary<Type, List<Callback>> eventListeners;
+
+
+        /// <summary>
+        /// Register a method to listen for an event.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="listener"></param>
+        public static void RegisterListener<T>(Action<T> listener) where T : EventInfo
+        {
+            //Get type to pass into the dictionary searchers/contstructor.
+            Type eventType = typeof(T);
+            //Reflection magic.
+            object target = listener.Target;
+            System.Reflection.MethodInfo method = listener.Method;
+            //Check if the dictionary, key and the corresponding value exist and if not, initialize them.
+            if(Instance.eventListeners == null)
+            {
+                Instance.eventListeners = new Dictionary<Type, List<Callback>>();
+            }
+            if(!Instance.eventListeners.ContainsKey(eventType) || Instance.eventListeners[eventType] == null)
+            {
+                Instance.eventListeners[eventType] = new List<Callback>();
+            }
+            //Construct the callback with reflection magic and add it to the event listener.
+            Callback callback = new Callback { target = target, method = method };
+            Instance.eventListeners[eventType].Add(callback);
+        }
+
+        /// <summary>
+        /// Stops method from listening to an event.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="listener"></param>
+        public static void RemoveListener<T>(Action<T> listener) where T : EventInfo
+        {
+            //If event dictionary hasn't been initialized, return.
+            if(Instance.eventListeners == null) { return; }
+            //Get type to pass into the dictionary searchers/contstructor.
+            Type eventType = typeof(T);
+            //Initialize eventListeners if they haven't been initialized yet.
+            if (Instance.eventListeners == null) Instance.eventListeners = new Dictionary<Type, List<Callback>>();
+            //If there no dictionary key or corresponding value exists, return.
+            if (!Instance.eventListeners.ContainsKey(eventType) || Instance.eventListeners[eventType] == null) { return; }
+            //Find and remove the callback.
+            Instance.eventListeners[eventType].RemoveAll(l => l.target == listener.Target && l.method == listener.Method);
+        }
+
+        /// <summary>
+        /// Fires an event with event information T. All listeners will execute corresponding methods.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="eventInfo"></param>
+        public static void Event<T>(T eventInfo) where T : EventInfo
+        {
+            //Get type to pass into the dictionary searchers/contstructor.
+            Type eventType = typeof(T);
+            //Check if the dictionary, key and the corresponding value exist and if not, initialize them.
+            if (Instance.eventListeners == null)
+            {
+                Instance.eventListeners = new Dictionary<Type, List<Callback>>();
+            }
+            //If there no dictionary key or corresponding value exists, return.
+            if (!Instance.eventListeners.ContainsKey(eventType) || Instance.eventListeners[eventType] == null) { return; }
+            //Call all listeners about the event.
+            for(int i = 0; i < Instance.eventListeners[eventType].Count; i++)
+            {
+                Instance.eventListeners[eventType][i].method.Invoke(Instance.eventListeners[eventType][i].target, new[] { eventInfo });
+            }
+            //foreach(Callback callback in Instance.eventListeners[eventType])
+            //{
+            //    callback.method.Invoke(callback.target, new[] { eventInfo });
+            //}
+        }
+
+        private class Callback 
+        {
+            public object target;
+            public System.Reflection.MethodInfo method;
+        }
+    }
+}
